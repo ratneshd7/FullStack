dfe3bbf1111c3a52609f8617430bf82ab5ac3954
Ratnesh Dubey <ratneshd786@gmail.com>
2023-11-06 01:01:53 +0530

    Code clean up

--- React/tic-tac-toe/src/game.tsx
+++ React/tic-tac-toe/src/game.tsx
@@ -2,21 +2,19 @@ import React, { useState } from "react";
 import Board from "./board.tsx";
 
 const Game = () => {
-  const [isXNext, setIsXNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[currentMove];
+  const isXNext = currentMove % 2 === 0;
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setIsXNext(!isXNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setIsXNext(nextMove % 2 === 0);
   }
 
   return (
